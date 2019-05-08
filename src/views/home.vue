63d2a4b39b71d5f4f431cce29e85ff4444c7dd22
phgap <2110370070@qq.com>
2019-05-08 19:13:40 +0800

    asaa

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,29 +1,21 @@
 <template>
     <div id="home">
-       <adv1></adv1>
-       <content1></content1>
+     
        <top></top>
        <maingoods></maingoods>
     </div>
 </template>
 <script>
 import top from './home/top'
-<<<<<<< HEAD
-import adv1 from './home/adv1'
-import content1 from './home/content1'
-export default {
-    components:{
-        top,
-        adv1,
-        content1
-=======
 import maingoods from './home/maingoods'
 import {mapState} from  'vuex'
+
 export default {
     components:{
         top,
-        maingoods
+     maingoods
     },
+
     computed:{
         ...mapState(['istabbarshow'])
     },
@@ -33,7 +25,6 @@ export default {
     },
     mounted(){
         this.$store.commit('istabbarshowmutation',true)
->>>>>>> 5c175ce38519403d1c460c756b889377a26f0389
     }
 }
 </script>
--- src/views/home/maingoods.vue
+++ src/views/home/maingoods.vue
@@ -1,8 +1,22 @@
 <template>
     <div id="maingoods">
-        <li v-for="n in 100">{{n}}</li>
+          <adv1></adv1>
+       <content1></content1>
     </div>
 </template>
+<script>
+import adv1 from './adv1'
+import content1 from './content1'
+
+export default {
+       components:{
+      
+        adv1,
+        content1,
+       
+    },
+}
+</script>
 <style lang='scss'>
     #maingoods{
         flex:1;
